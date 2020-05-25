c10806057d57acf89615f048f52f35ab051dfac9
Faraaz Malak <faraazmalak@gmail.com>
2020-05-25 12:58:14 +0530

    Adding user's full name to avatar's alt and title attributes

--- src/components/APIData/User/User.js
+++ src/components/APIData/User/User.js
@@ -2,14 +2,15 @@ import React from 'react';
 import "./User.scss"
 
 export function User(props){
+    const full_name = props.first_name + ' ' + props.last_name;
     return(
         <div className="user">
             <div className="column-container">
                 <div className="left column">
-                    <img  width="128" height="128" className="user-avatar" src={props.avatar} alt="avatar"/>
+                    <img  width="128" height="128" className="user-avatar" src={props.avatar} title={full_name} alt={full_name}/>
                 </div>
                 <div className="right column row-container">
-                    <span className="user-name row">{props.first_name + ' ' + props.last_name}</span>
+                    <span className="user-name row">{full_name}</span>
                     <span className="user-email row">{props.email}</span>
                 </div>
             </div>
